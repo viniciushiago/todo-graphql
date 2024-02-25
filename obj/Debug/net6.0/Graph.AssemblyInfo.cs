a4232c27b54f96f740a1b3a9253bdea02431503c
Vinicis Hiago da Rosa <viniciushiagor@gmail.com>
2024-02-25 18:11:44 -0300

    feat: adicionado banco aws

--- obj/Debug/net6.0/Graph.AssemblyInfo.cs
+++ obj/Debug/net6.0/Graph.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Graph")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c18e934d715ff20709b3c374602cbbc269e88e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0abf1294be7e00d3847a5c3917c3856d2297b79e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Graph")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Graph")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
